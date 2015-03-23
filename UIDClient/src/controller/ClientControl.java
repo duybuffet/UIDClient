2a26774c23438031113ea7d21c1de3ea47c6eb95
Duy Buffet <Duy Buffet@Buffet_probook>
2015-03-24 01:31:23 +0700

    Up client lan dau

--- UIDClient/src/controller/ClientControl.java
+++ UIDClient/src/controller/ClientControl.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package uidclient;
+package controller;
 
 /**
  *
  * @author Duy Buffet
  */
-public class UIDClient {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ClientControl {
     
 }
